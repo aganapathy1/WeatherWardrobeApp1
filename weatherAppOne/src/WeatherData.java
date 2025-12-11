23932f059756a3367a0ba609bc6d819c70aea1a2
aganapathy1 <aishwaryaganapathy4@gmail.com>
2025-12-11 07:47:43 -0700

    Update WeatherData.java

--- weatherAppOne/src/WeatherData.java
+++ weatherAppOne/src/WeatherData.java
@@ -1,9 +1,12 @@
 public class WeatherData {
+    //initialize variables
     String date;
     double temp;
     String conditions;
     double windspeed;
 
+    //basic constructor: will use the weather_data csv file and split the csv into the data, temp conditions and windspeed parts
+    //then it will store using this. for later calculations
     WeatherData(String date, double temp, String conditions, double windspeed) {
         this.date = date;
         this.temp = temp;
